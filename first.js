415ceaa9b1fa5e78c22b07a35075d6a4e32e2011
Ahmed Nasser <ahmedjsdev@gmail.com>
2018-05-10 17:19:43 +0200

    creating first.js

---
+++ first.js
@@ -0,0 +1,149 @@
+/* ************** Input ************** */
+var objectsArray = [
+    {
+        'row': 1,
+        'title': 'test 1',
+        'description': 'This is test num 1',
+    },
+    {
+        'row': 2,
+        'title': 'test 2',
+        'description': 'This is test num 2',
+    },
+    {
+        'row': 1,
+        'title': 'test 3',
+        'description': 'This is test num 3',
+    },
+    {
+        'row': 3,
+        'title': 'test 4',
+        'description': 'This is test num 4',
+    },
+    {
+        'row': 3,
+        'title': 'test 5',
+        'description': 'This is test num 5',
+    },
+    {
+        'row': 2,
+        'title': 'test 6',
+        'description': 'This is test num 6',
+    },
+    {
+        'row': 1,
+        'title': 'test 7',
+        'description': 'This is test num 7',
+    },
+    {
+        'row': 1,
+        'title': 'test 8',
+        'description': 'This is test num 8',
+    },
+    {
+        'row': 4,
+        'title': 'test 9',
+        'description': 'This is test num 9',
+    },
+    {
+        'row': 4,
+        'title': 'test 10',
+        'description': 'This is test num 10',
+    },
+    {
+        'row': 5,
+        'title': 'test 11',
+        'description': 'This is test num 11',
+    },
+    {
+        'row': 5,
+        'title': 'test 12',
+        'description': 'This is test num 12',
+    },
+    {
+        'row': 3,
+        'title': 'test 13',
+        'description': 'This is test 13',
+    }
+];
+
+/* ************** Output ************** */
+
+{
+  "1": [
+    {
+      "row": 1,
+      "title": "test 1",
+      "description": "This is test num 1"
+    },
+    {
+      "row": 1,
+      "title": "test 3",
+      "description": "This is test num 3"
+    },
+    {
+      "row": 1,
+      "title": "test 7",
+      "description": "This is test num 7"
+    },
+    {
+      "row": 1,
+      "title": "test 8",
+      "description": "This is test num 8"
+    }
+  ],
+  "2": [
+    {
+      "row": 2,
+      "title": "test 2",
+      "description": "This is test num 2"
+    },
+    {
+      "row": 2,
+      "title": "test 6",
+      "description": "This is test num 6"
+    }
+  ],
+  "3": [
+    {
+      "row": 3,
+      "title": "test 4",
+      "description": "This is test num 4"
+    },
+    {
+      "row": 3,
+      "title": "test 5",
+      "description": "This is test num 5"
+    },
+    {
+      "row": 3,
+      "title": "test 13",
+      "description": "This is test 13"
+    }
+  ],
+  "4": [
+    {
+      "row": 4,
+      "title": "test 9",
+      "description": "This is test num 9"
+    },
+    {
+      "row": 4,
+      "title": "test 10",
+      "description": "This is test num 10"
+    }
+  ],
+  "5": [
+    {
+      "row": 5,
+      "title": "test 11",
+      "description": "This is test num 11"
+    },
+    {
+      "row": 5,
+      "title": "test 12",
+      "description": "This is test num 12"
+    }
+  ]
+}
+
